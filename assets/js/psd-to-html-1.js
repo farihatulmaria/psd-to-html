575af4a199e3d12f25c0ffd0adbef4674a932bb4
farihatulmaria <farihatulmaria@gmail.com>
2021-09-09 17:03:02 +0600

    update

---
+++ assets/js/psd-to-html-1.js
@@ -0,0 +1,19 @@
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    $(".product-slider").slick({
+      dots: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+    $(".comment-slider").slick({
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  });
+})(jQuery);
