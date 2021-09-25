7ddbf213fa4093c944b52c2d2cea369da085cc72
farihatulmaria <farihatulmaria@gmail.com>
2021-09-25 15:18:27 +0600

    update

--- assets/js/psd-to-html13.js
+++ assets/js/psd-to-html13.js
@@ -7,12 +7,5 @@
       autoplay: true,
       autoplaySpeed: 1000,
     });
-    $(".comment-slider").slick({
-      dots: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 5000,
-    });
   });
 })(jQuery);
